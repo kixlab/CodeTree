137171cc45b7f790a0ffc5b750a561bdd3b1e0bc
jhw123 <hyoungwookjin123@gmail.com>
2022-12-07 19:32:05 +0900

    🐛 fix deploy time

--- backend/src/controller/getHealth.ts
+++ backend/src/controller/getHealth.ts
@@ -1,8 +1,8 @@
 import { Get } from '../HttpResponse'
 import { GetHealthParams, GetHealthResults } from '../protocol/GetHealth'
 
-const deployTime = Date.now()
+const deployTime = new Date()
 
-export const getHealthController = Get<GetHealthParams, GetHealthResults>(async params => {
-  return { status: deployTime.toLocaleString() }
+export const getHealthController = Get<GetHealthParams, GetHealthResults>(async () => {
+  return { status: deployTime.toString() }
 })
